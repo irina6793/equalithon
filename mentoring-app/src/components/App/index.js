b9987b2c31eca78c6fe939a1e203157b4291eb22
Katherine Pully <kpully@gmail.com>
2019-07-30 10:33:55 -0400

    adding navigation component

--- mentoring-app/src/components/App/index.js
+++ mentoring-app/src/components/App/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => (
-  <div>
+<Router>
+<Navigation />
+</Router> 
+ <div>
     <h1>App</h1>
   </div>
 );
